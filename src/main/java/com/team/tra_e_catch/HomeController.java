d958b43765f86f69a7ebe4293d6b3ce36a145201
Jin Lee <jinconan@gmail.com>
2018-09-03 14:32:10 +0900

    Dynamic Web Project -> Spring MVC Project로 변경

---
+++ src/main/java/com/team/tra_e_catch/HomeController.java
@@ -0,0 +1,22 @@
+package com.team.tra_e_catch;
+
+import java.util.Locale;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is");
+		return "home";
+	}
+	
+}
