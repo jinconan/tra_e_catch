086298e0f6a34e86a2cdbc42de3a4fb2d1496570
monotori <sco333@naver.com>
2018-09-06 18:20:47 +0900

    로컬명 : 작성자 : 신철우 날짜 : 2018-09-06 commit차수 : 1 변환사항 : 컨트롤러를 수정 세부내용 :  서브메뉴와 연결이 되는 컨트롤러를 수정 xml쪽도 변경

--- src/main/java/com/team/tra_e_catch/controller/AccountingController.java
+++ src/main/java/com/team/tra_e_catch/controller/AccountingController.java
@@ -17,22 +17,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AccountingController {
 	
 	private static final Logger logger = Logger.getLogger(AccountingController.class);
-
+	
+	/*
 	@RequestMapping(value = "/sal", method = RequestMethod.GET)
-	public String sal_main(Model mod,@RequestParam Map<String, Object> pMap) {
+	public String sal_main(Model mod, @RequestParam Map<String, Object> pMap) {
 		logger.info("sal진입");
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto-submenu.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("accounting-submenu.xml");
 		List<Map<String, Object>> subMenuList = (List<Map<String, Object>>) context.getBean("acc-submenu");
 		mod.addAttribute("curSubMenu", "기획서 리스트");
 		mod.addAttribute("subMenuList", subMenuList);
 		return "acc/sales/sal_main";
-	}
+	}*/
 	
 	
 	@RequestMapping(value = "/slip", method = RequestMethod.GET)
 	public String slip_main(Model mod,@RequestParam Map<String, Object> pMap) {
 		logger.info("slip진입");
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto-submenu.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("accounting-submenu.xml");
 		List<Map<String, Object>> subMenuList = (List<Map<String, Object>>) context.getBean("acc-submenu");
 		mod.addAttribute("curSubMenu", "기획서 리스트");
 		mod.addAttribute("subMenuList", subMenuList);
@@ -43,7 +44,7 @@ public class AccountingController {
 	@RequestMapping(value = "/tax", method = RequestMethod.GET)
 	public String tax_main(Model mod,@RequestParam Map<String, Object> pMap) {
 		logger.info("tax진입");
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto-submenu.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("accounting-submenu.xml");
 		List<Map<String, Object>> subMenuList = (List<Map<String, Object>>) context.getBean("acc-submenu");
 		mod.addAttribute("curSubMenu", "기획서 리스트");
 		mod.addAttribute("subMenuList", subMenuList);
@@ -54,7 +55,7 @@ public class AccountingController {
 	@RequestMapping(value = "/team", method = RequestMethod.GET)
 	public String team_main(Model mod,@RequestParam Map<String, Object> pMap) {
 		logger.info("team진입");
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto-submenu.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("accounting-submenu.xml");
 		List<Map<String, Object>> subMenuList = (List<Map<String, Object>>) context.getBean("acc-submenu");
 		mod.addAttribute("curSubMenu", "기획서 리스트");
 		mod.addAttribute("subMenuList", subMenuList);
@@ -65,7 +66,7 @@ public class AccountingController {
 	@RequestMapping(value = "/wel", method = RequestMethod.GET)
 	public String wel_main(Model mod,@RequestParam Map<String, Object> pMap) {
 		logger.info("wel진입");
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto-submenu.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("accounting-submenu.xml");
 		List<Map<String, Object>> subMenuList = (List<Map<String, Object>>) context.getBean("acc-submenu");
 		mod.addAttribute("curSubMenu", "기획서 리스트");
 		mod.addAttribute("subMenuList", subMenuList);
