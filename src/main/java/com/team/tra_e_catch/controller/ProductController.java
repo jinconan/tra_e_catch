35f04c7e06cb1bfa01917886e7b468e4f80d0985
shin <503@503com>
2018-09-07 13:20:06 +0900

    로컬명:wnddnr4 작성자 : 신중욱 날짜 : 2018-09-07 commit차수 : 2 변동사항 :

--- src/main/java/com/team/tra_e_catch/controller/ProductController.java
+++ src/main/java/com/team/tra_e_catch/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.team.tra_e_catch.controller;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,5 +20,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ProductController {
 	
 	private static final Logger logger = Logger.getLogger(ProductController.class);
+	
+	@RequestMapping(value="/prod/view/prodInven/{pageNumber}")
+	public String viewPropList(Model mod, @PathVariable int  pageNumber) {
+		logger.info("viewPropList(pageNumber="+pageNumber+")");
+		//컨트롤러로 부터 넘겨받는 속성
+		//subMenuList : List<Map<String, Object>>
+		//				[{key : value}] = [{"sm_name" : "서브메뉴이름"}, {"sm_url" : "링크경로"}]
+		//curSubMenu : String
+		ApplicationContext context = new ClassPathXmlApplicationContext("plan-submenu.xml");
+		List<Map<String,Object>> subMenuList = (List<Map<String,Object>>)context.getBean("prod-submenu");
+		mod.addAttribute("curSubMenu", "기획서 리스트");
+		mod.addAttribute("subMenuList", subMenuList);
+		
+		
+		return "plan/prod/prodInven";
+	}
+	
+	
 
 }
