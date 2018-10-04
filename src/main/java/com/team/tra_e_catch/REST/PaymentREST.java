83c1ebd4fa4b7d2af3384a7146dd49cf71bce179
Lee Jin <jinconan00@naver.com>
2018-10-05 03:39:33 +0900

    로컬명: master 작성자 : 이진 날짜 : 2018-10-05
    변동사항 :
    
    Merge branch 'wnddnr18'
    
    Conflicts:
    	src/main/java/com/team/tra_e_catch/REST/PaymentREST.java
    	src/main/java/com/team/tra_e_catch/payment/PaymentLogic.java
    	src/main/java/com/team/tra_e_catch/payment/SqlPayDao.java
    	src/main/resources/mybatis-mapper/payment.xml
    	target/m2e-wtp/web-resources/META-INF/maven/com.team/tra_e_catch/pom.properties

--- src/main/java/com/team/tra_e_catch/REST/PaymentREST.java
+++ src/main/java/com/team/tra_e_catch/REST/PaymentREST.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
